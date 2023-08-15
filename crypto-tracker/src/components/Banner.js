f8050a026a0f172599a53e75ab10c0c300db20f4
steven-nguyen22 <snguy117@ucr.edu>
2023-08-14 21:41:40 -0700

    first half of project

---
+++ crypto-tracker/src/components/Banner.js
@@ -0,0 +1,59 @@
+import { Container, Typography, makeStyles } from "@material-ui/core";
+import React from "react";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            varaint="h2"
+            style={{
+              frontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto Tracker
+          </Typography>
+          <Typography
+            varaint="subtitle12"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the info regarding your favorite Crypto Currencies
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
